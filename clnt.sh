f10013353372106be7d75ffda996788af5d50fc6
Neimhin <nrobinso@tcd.ie>
2024-03-25 11:24:23 +0000

    localhost test for sech

---
+++ clnt.sh
@@ -0,0 +1,1 @@
+$HOME/code/ech-dev-utils/scripts/echcli.sh --wait 0 -s localhost -H foo.example.com -p 8443 -P echconfig.pem -f index.html -- -sech_symmetric_key aa00
---
+++ svr.sh
@@ -0,0 +1,1 @@
+$HOME/code/ech-dev-utils/scripts/echsvr.sh $@ -- -sech_symmetric_key abababab
